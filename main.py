05236dec5ca53f93d104678d0585a47966d141a6
amanina01 <amaninafarhanah.mazlan@gmail.com>
2021-10-03 00:20:57 +0800

    let learn fastapi

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+
+
+app = FastAPI()
+
+
+
+
+
+@app.get("/")
+def index():
+    return{"data":{"name": "Amanina"}}
+
+@app.get("/about")
+def about():
+    return {"data":{"about page"}}
